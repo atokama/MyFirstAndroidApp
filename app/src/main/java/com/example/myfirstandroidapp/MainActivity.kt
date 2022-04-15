ff5a7fe6b13c43c3e2c419d53bdb3246602a9892
atoka <makrusan@gmail.com>
2022-04-15 11:45:25 +0530

    Add OpenCV dependency

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.myfirstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val p: Boolean = OpenCVLoader.initDebug()
+        Log.d("cvcv", "loaded: $p")
     }
 }
